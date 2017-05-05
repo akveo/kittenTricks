29131fbd8dd34600c7b68129ed0d28b1335957e4
Alexei Malashkevich <alexei.malashkevich@gmail.com>
2017-05-05 14:59:26 +0300

    feat(articles) used new page for articles v2

---
+++ kittenApp/app/screens/articles/articles2.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  ScrollView,
+  View
+} from 'react-native';
+import {RkButton} from 'react-native-ui-kitten';
+
+export class Aricles2 extends React.Component {
+  static navigationOptions = {
+    title: ''
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <View/>
+
+    )
+  }
+}
--- kittenApp/app/screens/articles/index.js
+++ kittenApp/app/screens/articles/index.js
@@ -1,1 +1,2 @@
 export * from './articles1';
+export * from './articles2';
