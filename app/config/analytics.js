a92b1d3a2a5c22bafc8ac4147efc2399ebdd47cc
Sergey Kozel <kozel.sergey@gmail.com>
2018-01-09 14:43:01 +0300

    fix(common): google analytics id fix

--- app/config/analytics.js
+++ app/config/analytics.js
@@ -1,6 +1,6 @@
 import { Analytics, PageHit } from 'expo-analytics';
 
-let google_analytics_id = 'UA-100868948-1';
+let google_analytics_id = 'UA-112172761-2';
 
 let analytics = new Analytics(google_analytics_id);
 
