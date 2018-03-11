8728360f77df5b46c4cbff812e0c8f8b88be2441
Zekai Oğuz Özalp <zekai.ozalp@asyasoft.net>
2018-03-11 19:43:31 +0300

    Workaround for the yellow box warning on login screen.

--- app/app.js
+++ app/app.js
@@ -14,6 +14,7 @@ import {View} from "react-native";
 
 bootstrap();
 data.populateData();
+console.ignoredYellowBox = ['Warning: Failed prop type: Invalid props.style key `fontSize` supplied to `View`'];
 
 function getCurrentRouteName(navigationState) {
   if (!navigationState) {
