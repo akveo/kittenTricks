e245852666a2be68cac5129c53066e0cbbb74b74
Artur Yorsh <artyorsh.me@gmail.com>
2018-08-25 21:49:10 +0300

    refactor(lint): config - analytics

--- app/config/analytics.js
+++ app/config/analytics.js
@@ -1,8 +1,6 @@
 import { Analytics, PageHit } from 'expo-analytics';
 
-const google_analytics_id = 'UA-112172761-2';
-
-const analytics = new Analytics(google_analytics_id);
+const analytics = new Analytics('UA-112172761-2');
 
 const track = screen => analytics.hit(new PageHit(screen));
 
