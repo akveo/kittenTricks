7ff0f8393db891508d362c13231fcc276f82fa16
Artur Yorsh <artyorsh.me@gmail.com>
2018-08-27 15:48:58 +0300

    refactor(lint): utils - remove unused utils

--- app/utils/colorUtils.js
+++
@@ -1,9 +0,0 @@
-hexToRGB = function (hex) {
-  const rgbHex = parseInt(hex.slice(1), 16);
-  const r = rgbHex >> 16;
-  const g = rgbHex >> 8 & 0xFF;
-  const b = rgbHex & 0xFF;
-  return `rgb(${r},${g},${b})`;
-};
-
-export default hexToRGB;
