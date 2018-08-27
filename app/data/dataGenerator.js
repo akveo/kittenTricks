ba754a29da365c0ad41f4ea36d133dc873fe7fc5
Artur Yorsh <artyorsh.me@gmail.com>
2018-08-27 10:22:07 +0300

    refactor(lint): data - dataGenerator

--- app/data/dataGenerator.js
+++ app/data/dataGenerator.js
@@ -1,31 +1,31 @@
+import _ from 'lodash';
 import users from './raw/users';
 import articles from './raw/articles';
 import notifications from './raw/notifications';
 import conversations from './raw/conversations';
-import _ from 'lodash';
 
 function populateArticles() {
-  for (const article of articles) {
-    const userId = articles.indexOf(article) % users.length;
-    article.user = _.find(users, x => x.id == userId) || users[0];
-    for (const comment of article.comments) {
-      const userId = article.comments.indexOf(comment) % users.length;
-      comment.user = _.find(users, x => x.id == userId) || users[0];
-    }
-  }
+  articles.forEach(article => {
+    const articleUserId = articles.indexOf(article) % users.length;
+    article.user = _.find(users, x => x.id === articleUserId) || users[0];
+    article.comments.forEach(comment => {
+      const commentUserId = article.comments.indexOf(comment) % users.length;
+      comment.user = _.find(users, x => x.id === commentUserId) || users[0];
+    });
+  });
 }
 
 function populateNotifications() {
-  for (const notification of notifications) {
+  notifications.forEach(notification => {
     const userId = notifications.indexOf(notification) % users.length;
-    notification.user = _.find(users, x => x.id == userId) || users[0];
-  }
+    notification.user = _.find(users, x => x.id === userId) || users[0];
+  });
 }
 
 function populateConversations() {
-  for (const conversation of conversations) {
-    conversation.withUser = _.find(users, x => x.id == conversation.withUserId) || users[0];
-  }
+  conversations.forEach(conversation => {
+    conversation.withUser = _.find(users, x => x.id === conversation.withUserId) || users[0];
+  });
 }
 
 const populate = () => {
