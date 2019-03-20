f12dcb6be4e1b658cbc405b322106dc520c41f70
Yauhen Penkin <e.penkin@akveo.com>
2019-03-20 16:11:05 +0300

    feat(app): project setup

--- app/screens/articles/index.js
+++
@@ -1,6 +0,0 @@
-export * from './articles1';
-export * from './articles2';
-export * from './articles3';
-export * from './articles4';
-export * from './blogposts';
-export * from './article';
--- app/screens/social/index.js
+++
@@ -1,7 +0,0 @@
-export * from './profile1';
-export * from './profile2';
-export * from './profile3';
-export * from './profileSettings';
-export * from './notifications';
-export * from './contacts';
-export * from './feed';
