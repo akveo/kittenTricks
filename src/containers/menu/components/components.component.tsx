d011f72be08f3a243518b0d1a0c2293e4fd8f590
Artur Yorsh <10753921+artyorsh@users.noreply.github.com>
2019-04-20 06:03:59 +0300

    feat(layout): add components container

--- src/containers/menu/components/components.component.tsx
+++ src/containers/menu/components/components.component.tsx
@@ -1,26 +1,92 @@
 import React from 'react';
-import { View } from 'react-native';
 import {
-  ThemedComponentProps,
-  ThemeType,
+  ListRenderItemInfo,
+  View,
+} from 'react-native';
+import {
   withStyles,
+  ThemeType,
+  ThemedComponentProps,
 } from '@kitten/theme';
+import {
+  Input,
+  List,
+  ListItemProps,
+} from '@kitten/ui';
+import { SearchIconOutline } from '@src/assets/icons';
+import { ListItem } from './components.container';
+import { ComponentsListItem } from './componentsListItem.component';
 
-type Props = ThemedComponentProps;
+interface ComponentProps {
+  items: ListItem[];
+  onQueryChange: (query: string) => void;
+  onItemSelect: (index: number) => void;
+}
+
+type Props = ThemedComponentProps & ComponentProps;
 
 class ComponentsComponent extends React.Component<Props> {
 
-  public render(): React.ReactNode {
-    const { themedStyle } = this.props;
+  private onSearchInputTextChange = (query: string) => {
+    this.props.onQueryChange(query);
+  };
+
+  private onItemPress = (index: number) => {
+    this.props.onItemSelect(index);
+  };
+
+  private renderItem = (info: ListRenderItemInfo<ListItem>): React.ReactElement<ListItemProps> => {
+    const { title, view } = info.item;
 
     return (
-      <View style={themedStyle.container}/>
+      <ComponentsListItem
+        style={this.props.themedStyle.item}
+        title={title}
+        onPress={() => this.onItemPress(info.index)}>
+        {view()}
+      </ComponentsListItem>
+    );
+  };
+
+  public render(): React.ReactNode {
+    const { themedStyle, items } = this.props;
+
+    return (
+      <View style={themedStyle.container}>
+        <Input
+          style={themedStyle.searchInput}
+          placeholder='Search'
+          icon={SearchIconOutline}
+          onChangeText={this.onSearchInputTextChange}
+        />
+        <List
+          contentContainerStyle={themedStyle.list}
+          numColumns={2}
+          data={items}
+          renderItem={this.renderItem}
+        />
+      </View>
     );
   }
 }
 
 export const Components = withStyles(ComponentsComponent, (theme: ThemeType) => ({
   container: {
+    paddingVertical: 24,
+    paddingHorizontal: 16,
+    backgroundColor: theme['color-basic-100'],
+  },
+  searchInput: {
+    marginHorizontal: 8,
+    backgroundColor: theme['color-white'],
+  },
+  list: {
+    paddingVertical: 24,
+  },
+  item: {
     flex: 1,
+    height: 160,
+    marginHorizontal: 8,
+    marginVertical: 8,
   },
 }));
