6438e194e0520e5612735e0a68d265172629ae85
Artur Yorsh <artyorsh.me@gmail.com>
2019-04-23 01:36:10 +0300

    fix(layout): product details

--- src/components/ecommerce/index.ts
+++ src/components/ecommerce/index.ts
@@ -28,6 +28,10 @@ export {
   CartProductProps,
 } from './cartProduct.component';
 export {
+  ProductInfo,
+  ProductInfoProps,
+} from './productInfo.component';
+export {
   ProductList,
   ProductListProps,
 } from './productList/productList.component';
