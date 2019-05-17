911bde414c4861584d69f457e0f57bc8d12d86be
Artur Yorsh <10753921+artyorsh@users.noreply.github.com>
2019-05-17 11:57:15 +0300

    package(common): update typing packages versions (#125)

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,39 @@
-module.exports = function(api) {
+const path = require('path');
+
+// FIXME: Resolve `transform[stderr]: Could not resolve` command-line warnings.
+// FIXME: Reproducible when starting with clearing cache (npm start -- -c)
+//
+// TODO: Framework path aliasing even not needed here. Replace?
+// TODO: Replace nested package.json-s with aliases
+
+const moduleResolverConfig = {
+  root: path.resolve('./'),
+  alias: {
+    '@kitten/theme': path.resolve('./node_modules/react-native-ui-kitten'),
+    '@kitten/ui': path.resolve('./node_modules/react-native-ui-kitten'),
+  },
+};
+
+module.exports = function (api) {
   api.cache(true);
-  return {
-    presets: [
-      'babel-preset-expo'
-    ],
-    env: {
-      development: {
-        plugins: [
-          '@babel/transform-react-jsx-source',
-        ]
-      }
-    }
+
+  const presets = [
+    'babel-preset-expo',
+  ];
+
+  const plugins = [
+    ['module-resolver', moduleResolverConfig],
+  ];
+
+  const devPlugins = [
+    '@babel/transform-react-jsx-source',
+  ];
+
+  const env = {
+    development: {
+      plugins: devPlugins,
+    },
   };
+
+  return { presets, plugins, env };
 };
