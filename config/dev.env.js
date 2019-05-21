08a1e54f6e1881bc0c875f59b22a1ed15bf0cd77
Artur Yorsh <10753921+artyorsh@users.noreply.github.com>
2019-05-21 11:53:56 +0300

    fix(layout): ui-kitten adopt

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,4 +5,5 @@ module.exports = {
   KITTEN_PATH: path.resolve(__dirname, '../../react-native-ui-kitten'),
   MAPPING_PATH: path.resolve(__dirname, '../../eva/packages/mapping/eva'),
   THEME_PATH: path.resolve(__dirname, '../../eva/packages/theme/eva'),
+  PROCESSOR_PATH: path.resolve(__dirname, '../../eva/packages/processor/kitten'),
 };
