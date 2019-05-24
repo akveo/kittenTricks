5103e9f2ee846699d6ad05a2fbf2b41224ca5b03
Yauhen Penkin <e.penkin@akveo.com>
2019-05-24 10:04:47 +0300

    refactor(components): components menu refactor

--- src/containers/menu/components/components.container.tsx
+++ src/containers/menu/components/components.container.tsx
@@ -14,34 +14,16 @@ export class ComponentsContainer extends React.Component<NavigationScreenProps, 
     data: routes,
   };
 
-  private query: string = '';
-
-  private onQueryChange = (query: string) => {
-    this.query = query;
-
-    const data: ComponentsContainerData[] = routes.filter(this.fitsQuery);
-
-    this.setState({ data });
-  };
-
   private onItemSelect = (index: number) => {
     const { [index]: selectedItem } = this.state.data;
 
     this.props.navigation.navigate(selectedItem.route);
   };
 
-  private fitsQuery = (item: ComponentsContainerData): boolean => {
-    const title: string = item.title.toLowerCase();
-    const query: string = this.query.toLowerCase();
-
-    return title.includes(query);
-  };
-
   public render(): React.ReactNode {
     return (
       <Components
         data={this.state.data}
-        onQueryChange={this.onQueryChange}
         onItemSelect={this.onItemSelect}
       />
     );
