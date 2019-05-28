2dafe0a1982f1527da4ded9162bd5f3c6ac30c0a
Artur Yorsh <artyorsh.me@gmail.com>
2019-05-28 20:13:41 +0300

    refactor(package): adopt to eva

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   ENV: 'dev',
   KITTEN_PATH: path.resolve(__dirname, '../../react-native-ui-kitten'),
-  MAPPING_PATH: path.resolve(__dirname, '../../eva/packages/mapping/eva'),
-  THEME_PATH: path.resolve(__dirname, '../../eva/packages/themes'),
-  PROCESSOR_PATH: path.resolve(__dirname, '../../eva/packages/processor/kitten'),
+  MAPPING_PATH: path.resolve(__dirname, '../../eva/packages/eva'),
+  PROCESSOR_PATH: path.resolve(__dirname, '../../eva/packages/processor'),
 };
