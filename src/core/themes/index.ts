79a2d47291ac984a2444bfb74f5a63490f046b87
Artur Yorsh <10753921+artyorsh@users.noreply.github.com>
2019-06-04 11:41:24 +0300

    feat(theme): create app theme

--- src/core/themes/index.ts
+++ src/core/themes/index.ts
@@ -2,11 +2,13 @@ import {
   dark,
   light,
 } from '@eva-design/eva';
+import { default as appTheme } from './appTheme.json';
 import { ThemeType } from '@kitten/theme';
 
 interface ThemeRegistry {
   ['Eva Light']: ThemeType;
   ['Eva Dark']: ThemeType;
+  ['App Theme']: ThemeType;
 }
 
 export type ThemeKey = keyof ThemeRegistry;
@@ -14,6 +16,7 @@ export type ThemeKey = keyof ThemeRegistry;
 export const themes: ThemeRegistry = {
   'Eva Light': light,
   'Eva Dark': dark,
+  'App Theme': appTheme,
 };
 
 export {
