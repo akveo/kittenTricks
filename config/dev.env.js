57e51015a6ebd3ff8b25afc62b0bf93eb24c0975
Artur Yorsh <10753921+artyorsh@users.noreply.github.com>
2019-06-10 12:28:20 +0300

    build(common): update expo to sdk-33

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   ENV: 'dev',
-  KITTEN_PATH: path.resolve(__dirname, '../../react-native-ui-kitten'),
+  KITTEN_PATH: path.resolve(__dirname, '../../react-native-ui-kitten/src/framework'),
   MAPPING_PATH: path.resolve(__dirname, '../../eva/packages/eva'),
   PROCESSOR_PATH: path.resolve(__dirname, '../../eva/packages/processor'),
 };
--- metro.config.js
+++ metro.config.js
@@ -12,12 +12,9 @@ module.exports = {
       get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
     }),
   },
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-typescript-transformer'),
-  },
   watchFolders: [
-    path.resolve(Config.KITTEN_PATH, 'src/framework/theme'),
-    path.resolve(Config.KITTEN_PATH, 'src/framework/ui'),
+    path.resolve(Config.KITTEN_PATH, 'theme'),
+    path.resolve(Config.KITTEN_PATH, 'ui'),
     path.resolve(Config.MAPPING_PATH),
     path.resolve(Config.PROCESSOR_PATH),
   ],
