af8349824116d0a254ad5228e74f2baed8837f95
Yauhen <e.penkin@akveo.com>
2019-06-11 16:40:58 +0300

    release: v2.0.0

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   ENV: 'prod',
-  KITTEN_PATH: path.resolve(__dirname, '../node_modules/react-native-ui-kitten/src/framework'),
+  KITTEN_PATH: path.resolve(__dirname, '../node_modules/react-native-ui-kitten'),
   MAPPING_PATH: path.resolve(__dirname, '../node_modules/@eva-design/eva'),
   PROCESSOR_PATH: path.resolve(__dirname, '../node_modules/@eva-design/processor'),
 };
