7cc0f33edd8c4b60348727f071e734d47d396ede
Artur Yorsh <10753921+artyorsh@users.noreply.github.com>
2019-06-13 11:41:36 +0300

    fix(component): update container-view documentation

--- src/components/common/containerView.component.tsx
+++ src/components/common/containerView.component.tsx
@@ -9,7 +9,7 @@ export type ContainerViewProps = ScrollViewProps;
 /**
  * React Native ScrollView component, but modified to remove bounces by default
  *
- * Used everywhere per app, where content needs to be scrollable to feet layout to device screen
+ * Used everywhere per app, where content needs to be scrollable to fit layout to device screen
  */
 export class ContainerView extends React.Component<ContainerViewProps> {
 
