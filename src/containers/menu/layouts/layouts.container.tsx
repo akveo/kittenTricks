9423176c76ae9973c3c4c821c4e644cf349aabbf
Artur Yorsh <artyorsh.me@gmail.com>
2019-06-14 19:42:59 +0300

    fix(core): prevent multiple navigation

--- src/containers/menu/layouts/layouts.container.tsx
+++ src/containers/menu/layouts/layouts.container.tsx
@@ -3,6 +3,7 @@ import { NavigationScreenProps } from 'react-navigation';
 import { Layouts } from './layouts.component';
 import { LayoutsContainerData } from './type';
 import { routes } from './routes';
+import { navigateAction } from '@src/core/navigation';
 
 export class LayoutsContainer extends React.Component<NavigationScreenProps> {
 
@@ -11,10 +12,7 @@ export class LayoutsContainer extends React.Component<NavigationScreenProps> {
   private onItemSelect = (index: number) => {
     const { [index]: selectedItem } = this.data;
 
-    this.props.navigation.navigate({
-      key: 'Layouts',
-      routeName: selectedItem.route,
-    });
+    this.props.navigation.dispatch(navigateAction(selectedItem.route));
   };
 
   public render(): React.ReactNode {
