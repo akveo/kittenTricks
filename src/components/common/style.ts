b5ab1777c5f744a375133e49747340295d825555
Yauhen Penkin <e.penkin@akveo.com>
2019-06-18 16:34:36 +0300

    fix(app): fonts platform-dependent applying fix (rn-issue)

--- src/components/common/style.ts
+++ src/components/common/style.ts
@@ -1,23 +1,26 @@
+import { Platform } from 'react-native';
+import { StyleType } from '@kitten/theme';
+
 export const textStyle = {
-  headline: {
-    fontFamily: 'opensans-bold',
-  },
-  subtitle: {
-    fontFamily: 'opensans-semibold',
-  },
-  paragraph: {
-    fontFamily: 'opensans-regular',
-  },
-  caption1: {
-    fontFamily: 'opensans-regular',
-  },
-  caption2: {
-    fontFamily: 'opensans-semibold',
-  },
-  label: {
-    fontFamily: 'opensans-bold',
-  },
+  headline: getPlatformDependentFontStyle('opensans-bold', 'normal'),
+  subtitle: getPlatformDependentFontStyle('opensans-semibold', 'normal'),
+  paragraph: getPlatformDependentFontStyle('opensans-regular', 'normal'),
+  caption1: getPlatformDependentFontStyle('opensans-regular', 'normal'),
+  caption2: getPlatformDependentFontStyle('opensans-semibold', 'normal'),
+  label: getPlatformDependentFontStyle('opensans-bold', 'normal'),
   button: {
     // fontFamily: 'opensans-extrabold',
   },
 };
+
+function getPlatformDependentFontStyle(fontFamily: string, fontWeight: string): StyleType {
+  return Platform.select({
+    ios: {
+      fontFamily: fontFamily,
+    },
+    android: {
+      fontFamily: fontFamily,
+      fontWeight: fontWeight,
+    },
+  });
+}
