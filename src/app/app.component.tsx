8eabc166936b602d2a381e9010662e30135f95eb
Artur Yorsh <10753921+artyorsh@users.noreply.github.com>
2020-01-22 13:35:57 +0300

    fix: theme switch using system appearance

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { mapping } from '@eva-design/eva';
-import { ApplicationProvider, ApplicationProviderProps, IconRegistry } from '@ui-kitten/components';
+import { IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { ApplicationLoader, Assets } from './app-loader.component';
-import { default as appMapping } from './app-mapping.json';
-import { appThemes } from './app-themes';
 import { AppIconsPack } from './app-icons-pack';
+import { AppProvider } from './app-provider.component';
 import { StatusBar } from '../components/status-bar.component';
 import { AppNavigator } from '../navigation/app.navigator';
-import { Theming } from '../services/theme.service';
 
 const assets: Assets = {
   fonts: {
@@ -17,30 +13,14 @@ const assets: Assets = {
   },
 };
 
-export default (): React.ReactElement => {
-
-  const [themeContext, theme] = Theming.useTheming(appThemes, 'light');
-
-  const appConfig: ApplicationProviderProps = {
-    mapping: mapping,
-    theme: theme,
-    // @ts-ignore
-    customMapping: appMapping,
-  };
-
-  return (
-    <ApplicationLoader
-      assets={assets}
-      splash={require('../assets/images/image-splash.png')}>
-      <IconRegistry icons={[EvaIconsPack, AppIconsPack]}/>
-      <Theming.Context.Provider value={themeContext}>
-        <ApplicationProvider {...appConfig}>
-          <SafeAreaProvider>
-            <StatusBar/>
-            <AppNavigator/>
-          </SafeAreaProvider>
-        </ApplicationProvider>
-      </Theming.Context.Provider>
-    </ApplicationLoader>
-  );
-};
+export default (): React.ReactElement => (
+  <ApplicationLoader
+    assets={assets}
+    splash={require('../assets/images/image-splash.png')}>
+    <IconRegistry icons={[EvaIconsPack, AppIconsPack]}/>
+    <AppProvider initialTheme='light'>
+      <StatusBar/>
+      <AppNavigator/>
+    </AppProvider>
+  </ApplicationLoader>
+);
