39ec20b10aa2ed0cdfe62c89eb0dbf636ec91d4a
Artur Yorsh <10753921+artyorsh@users.noreply.github.com>
2020-02-07 11:39:56 +0300

    chore: upgrade dependencies

--- src/navigation/app.navigator.tsx
+++ src/navigation/app.navigator.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { NavigationNativeContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { HomeNavigator } from './home.navigator';
 
 export const AppNavigator = (): React.ReactElement => (
-  <NavigationNativeContainer>
+  <NavigationContainer>
     <HomeNavigator/>
-  </NavigationNativeContainer>
+  </NavigationContainer>
 );
