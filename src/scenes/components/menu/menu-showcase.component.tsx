b715f7c457a53542efa462b2ecadde51f257b03d
Ivan Vezhnavets <ivanvezhnavets@MacBook-Pro-Ivan.local>
2021-01-04 22:28:50 +0300

    migration: update menu component to v5

--- src/scenes/components/menu/menu-showcase.component.tsx
+++ src/scenes/components/menu/menu-showcase.component.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Menu, MenuElement, MenuProps } from '@ui-kitten/components';
+import { IndexPath, Menu, MenuElement, MenuProps } from '@ui-kitten/components';
 
 export const MenuShowcase = (props: MenuProps): MenuElement => {
 
   const [selectedIndex, setSelectedIndex] = React.useState(null);
 
-  const onSelect = (index: number): void => {
-    setSelectedIndex(index);
+  const onSelect = (index: IndexPath): void => {
+    setSelectedIndex(index.row);
   };
 
   return (
