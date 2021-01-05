1a54b4aa8e248b73f0a239a119ff2a7e5c876b71
Ivan Vezhnavets <ivanvezhnavets@MacBook-Pro-Ivan.local>
2021-01-05 19:38:56 +0300

    fix: fix input & menu components

--- src/scenes/components/menu/menu-showcase.component.tsx
+++ src/scenes/components/menu/menu-showcase.component.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import { IndexPath, Menu, MenuElement, MenuProps } from '@ui-kitten/components';
+import React, { ReactElement } from 'react';
+import { IndexPath, Menu, MenuElement, MenuProps, MenuItem, MenuGroup } from '@ui-kitten/components';
+import { MenuGroupType } from './type';
 
-export const MenuShowcase = (props: MenuProps): MenuElement => {
+interface MenuShowcaseProps extends MenuProps {
+  data: MenuGroupType[],
+}
+
+export const MenuShowcase = (props: MenuShowcaseProps): MenuElement => {
 
   const [selectedIndex, setSelectedIndex] = React.useState(null);
 
@@ -9,11 +14,23 @@ export const MenuShowcase = (props: MenuProps): MenuElement => {
     setSelectedIndex(index.row);
   };
 
+  const renderData: MenuElement[] = props.data.map((el, index) => (
+    el.subItems ? (
+      <MenuGroup key={index}>
+        {el.subItems.map((el, index) => (
+          <MenuItem key={index} {...el} />
+        ))}
+      </MenuGroup>
+    ) : <MenuItem key={index} {...el} />
+  ));
+
   return (
     <Menu
       {...props}
       selectedIndex={selectedIndex}
       onSelect={onSelect}
-    />
+    >
+      {renderData}
+    </Menu>
   );
 };
