720002ccebbd438812c72fb95ea0994a9430072a
Ivan Vezhnavets <ivanvezhnavets@MacBook-Pro-Ivan.local>
2021-01-05 14:44:32 +0300

    fix: autocompleme component fix

--- src/scenes/components/autocomplete/autocomplete-examples.tsx
+++ src/scenes/components/autocomplete/autocomplete-examples.tsx
@@ -3,8 +3,8 @@ import { View, StyleSheet } from 'react-native';
 import { Avatar, Text, AutocompleteItem } from '@ui-kitten/components';
 import { DataProps } from './type';
 
-export const CustomOptionsAutocompleteItem = ({ title, releaseYear }: DataProps): any => (
-  <AutocompleteItem style={styles.wrapper}>
+export const CustomOptionsAutocompleteItem = ({ title, releaseYear, id }: DataProps): any => (
+  <AutocompleteItem style={styles.wrapper} key={id}>
     <Avatar size='small' source={require('../../../assets/images/image-app-icon.png')}/>
     <View style={{ marginHorizontal: 8 }}>
       <Text>{title}</Text>
